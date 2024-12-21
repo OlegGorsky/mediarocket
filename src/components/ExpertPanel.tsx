253be996ec886593a15bde2244234291f5a3d064
Gorsky <my@gorskymail.ru>
2024-12-21 13:34:30 +0300

    Обновил компонент RatingPage и RatingCard

--- src/components/ExpertPanel.tsx
+++ src/components/ExpertPanel.tsx
@@ -30,8 +30,6 @@ export const ExpertPanel: React.FC<ExpertPanelProps> = ({
       const channelUsername = expert.link.split('/').pop() || '';
       const response = await api.checkSubscription(user.id, channelUsername);
 
-      console.log('Subscription response:', response); // Логирование ответа
-
       const subscriptionStatus = response.subscribe; // Извлекаем статус подписки из ответа
 
       switch (subscriptionStatus) {
@@ -52,7 +50,6 @@ export const ExpertPanel: React.FC<ExpertPanelProps> = ({
           toast.error('Неизвестный статус подписки.');
       }
     } catch (error) {
-      console.error('Error checking subscription:', error); // Логирование ошибки
       toast.error('Произошла ошибка при проверке подписки');
     }
   };
